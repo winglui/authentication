57d727f80b5c9c8c1d70f48a9952d545af2786a6
Wing Lui <winglui@gmail.com>
2023-11-06 09:20:21 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+const port = 3000;
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const secret = "THEYANKEESARETHEBEST";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+const User = new mongoose.model("User", userSchema);
+
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", async function(req, res){
+
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    try {
+        await newUser.save();
+        res.render("secrets");
+    } catch (err) {
+        console.log(err);
+    }
+
+});
+
+app.post("/login", async function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        var foundUser = await User.findOne({email: username});
+        if (foundUser) {
+            if (foundUser.password === password) {
+                res.render("secrets");
+            }                
+        }
+    } catch (err) {
+        console.log(err);
+    }
+
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+  
